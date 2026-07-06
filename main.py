39b379d1d7c3692b7fc6a489d42348ff8e36a463
Ahmadw4473 <ahmadw4473@gmail.com>
2026-07-06 18:52:47 +0500

    saving paths of images inside main folder adn sub folders

---
+++ main.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+folder_path = Path.home() / "Pictures"
+
+extensions = {".jpg", ".png", ".jpeg"}
+images = []
+for path in folder_path.rglob("*"):
+    if path.is_file() and path.suffix.lower() in extensions:
+        images.append(path)
+
+print(images[:10])
